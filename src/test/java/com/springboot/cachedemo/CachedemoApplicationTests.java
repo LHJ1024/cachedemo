f9b480c4efa7b821e0adddd1e964a48dfb6ef4c8
lhj1024 <18875143382@163.com>
2019-12-09 17:27:12 +0800

    配置mysql server时区

--- src/test/java/com/springboot/cachedemo/CachedemoApplicationTests.java
+++ src/test/java/com/springboot/cachedemo/CachedemoApplicationTests.java
@@ -1,14 +1,29 @@
 package com.springboot.cachedemo;
 
+import com.springboot.cachedemo.bean.Employee;
+import com.springboot.cachedemo.mapper.EmployeeMapper;
 import org.junit.Test;
 //import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class CachedemoApplicationTests {
+public class CachedemoApplicationTests {
+    @Autowired
+    EmployeeMapper employeeMapper;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+        Employee getbyid
+//                = new Employee();
+                = employeeMapper.getEmpById(2);
+        System.out.print(getbyid);
+//        getbyid.setLastName("wayne");
+//        employeeMapper.insertEmployee(getbyid);
+        //employeeMapper.deleteEmpById(1);
+    }
 
 }
