43d6946b70c84ffe20320dd3e20a3a13d38b80cf
lhj1024 <18875143382@163.com>
2019-12-09 17:57:13 +0800

    配置cacheconfig,整合controller和service

--- src/main/java/com/springboot/cachedemo/CachedemoApplication.java
+++ src/main/java/com/springboot/cachedemo/CachedemoApplication.java
@@ -3,8 +3,11 @@ package com.springboot.cachedemo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
 @MapperScan("com.springboot.cachedemo.mapper")
 @SpringBootApplication
+@EnableCaching
 public class CachedemoApplication {
 
 	public static void main(String[] args) {
