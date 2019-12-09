ff975d34c85da67d6fefb5d801aaa900e1eb6cc8
lhj1024 <18875143382@163.com>
2019-12-09 11:23:33 +0800

    Initial commit

---
+++ src/test/java/com/springboot/cachedemo/CachedemoApplicationTests.java
@@ -0,0 +1,14 @@
+package com.springboot.cachedemo;
+
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CachedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
