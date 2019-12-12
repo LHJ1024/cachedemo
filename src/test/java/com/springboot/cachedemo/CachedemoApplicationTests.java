f7594b12f660c7f9ed7f7e60543c9398c5fccf0e
lhj1024 <18875143382@163.com>
2019-12-12 10:17:23 +0800

    配置redis

--- src/test/java/com/springboot/cachedemo/CachedemoApplicationTests.java
+++ src/test/java/com/springboot/cachedemo/CachedemoApplicationTests.java
@@ -7,6 +7,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
@@ -14,7 +16,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class CachedemoApplicationTests {
     @Autowired
     EmployeeMapper employeeMapper;
+    @Autowired
+    StringRedisTemplate stringRedisTemplate;  //操作k-v都是字符串的
 
+    @Autowired
+    RedisTemplate redisTemplate;  //k-v都是对象的
+
+//    @Autowired
+//    RedisTemplate<Object, Employee> empRedisTemplate;
+
+    @Test
+    public void test01(){
+       // stringRedisTemplate.opsForValue().append("msg","hello");
+		String msg = stringRedisTemplate.opsForValue().get("msg");
+		System.out.println(msg);
+    }
     @Test
     public void contextLoads() {
         Employee getbyid
@@ -24,6 +40,11 @@ public class CachedemoApplicationTests {
 //        getbyid.setLastName("wayne");
 //        employeeMapper.insertEmployee(getbyid);
         //employeeMapper.deleteEmpById(1);
+    }
+    @Test
+    public  void test02(){
+        Employee emp = employeeMapper.getEmpById(2);
+        redisTemplate.opsForValue().set("emp-01", emp);
     }
 
 }
